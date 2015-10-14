0ae89b650f55f7a2d6cdc229c231c07ac1921722
unknown <amy@origamistudios.us>
2015-10-14 12:38:01 +0500

    Project Initialization
    Contains Only the files for Laravel framework

---
+++ app/tests/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestCase
+ *
+ * @author kas
+ */
+class TestCase extends Illuminate\Foundation\Testing\TestCase {
+
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+	 */
+	public function createApplication()
+	{
+		$unitTesting = true;
+
+		$testEnvironment = 'testing';
+
+		return require __DIR__.'/../../bootstrap/start.php';
+	}
+
+}
